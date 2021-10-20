9206c3bc160875ac9f0cb4cf1827b8d92678d9a8
perchespierri <perchespierri@gmail.com>
2021-10-20 19:07:55 -0300

    Reiniciando e limpando commits

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
-// import logo from './trivia.png';
+import React from 'react';
+import logo from './trivia.png';
 import './App.css';
-import Login from './pages/Login';
 
-class App extends Component {
-  render() {
-    return (
-      <Switch>
-        <Route exact path="/" component={ Login } />
-      </Switch>
-    );
-  }
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={ logo } className="App-logo" alt="logo" />
+        <p>
+          SUA VEZ
+        </p>
+      </header>
+    </div>
+  );
 }
-
-export default App;
