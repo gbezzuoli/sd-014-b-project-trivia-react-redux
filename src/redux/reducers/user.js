864557950cc2bdbd5bbf9b700692a9e3847f3192
belaguarino <izabelaguarino@gmail.com>
2021-10-20 16:56:50 -0300

    Atualizando o stage

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,9 +1,5 @@
-import {
-  FAILED_REQUEST,
-  GET_TOKEN,
-  REQUEST_API,
-  SAVE_USER,
-} from '../actions/actionTypes';
+import { FAILED_REQUEST,
+  GET_TOKEN, REQUEST_API, SAVE_USER } from '../actions/actionTypes';
 
 const INITIAL_STATE = {
   name: '',
@@ -15,7 +11,28 @@ const INITIAL_STATE = {
 const user = (state = INITIAL_STATE, action) => {
   switch (action.type) {
   case SAVE_USER:
-    return { name: action.name, email: action.email };
+    return {
+      ...state,
+      name: action.name,
+      email: action.email,
+    };
+  case REQUEST_API:
+    return {
+      ...state,
+      isLoading: true,
+    };
+  case FAILED_REQUEST:
+    return {
+      ...state,
+      isLoading: false,
+      error: action.error,
+    };
+  case GET_TOKEN:
+    return {
+      ...state,
+      isLoading: false,
+      token: action.token,
+    };
   default:
     return state;
   }
