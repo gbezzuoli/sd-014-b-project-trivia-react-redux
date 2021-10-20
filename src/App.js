9112c4b01453ec0869f6350337786a90ec83fd7a
perchespierri <perchespierri@gmail.com>
2021-10-20 19:43:32 -0300

    Pagina de login criada, store importada em index e path para login adicionado em app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import logo from './trivia.png';
+import { Route, Switch } from 'react-router-dom';
+// import logo from './trivia.png';
+import Login from './pages/Login';
 import './App.css';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Iniciando
-        </p>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/" component={ Login } />
+    </Switch>
   );
 }
