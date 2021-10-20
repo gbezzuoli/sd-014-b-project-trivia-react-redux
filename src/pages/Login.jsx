4386c44dad7e08cacbcbbc0d677e9f4ab45394cb
rphsantos1995 <rph.santos@Outlook.com>
2021-10-20 17:12:01 -0300

    Requisito 1

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -19,16 +19,23 @@ class Login extends Component {
     });
   }
 
+  // isEmailValid = (email) => {
+  //   const regexEmail = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  //   return regexEmail.test(email) === true;
+  // };
+
 
 
   render() {
+    const { nameInput, emailInput } = this.state;
+    const validInput = nameInput.length > 0 && emailInput.length > 0;
     return (
       <div>
         <label htmlFor="input-player-name" />Nome
         <input name="nameInput" onChange={this.handleChange} data-testid="input-player-name" type="text" />
         <label htmlFor="input-player-name" />Email
         <input name="emailInput" onChange={this.handleChange} data-testid="input-gravatar-email" type="text" />
-        <button data-testid="btn-play">Jogar</button>
+        <button disabled={!validInput} data-testid="btn-play">Jogar</button>
       </div>
     );
   }
