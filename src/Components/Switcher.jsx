c795e55780f910165443ce75942398a67d9727f4
Antônio Gonçalves <tonygoldmarket@yahoo.com>
2021-10-20 20:57:17 -0300

    Requisito 03 concluido

--- src/Components/Switcher.jsx
+++ src/Components/Switcher.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from '../Pages/Login';
+import Settings from '../Pages/Settings';
 
 export default class Switcher extends Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route path="/settings" component={ Settings } />
       </Switch>
     );
   }
---
+++ src/Pages/Settings.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Settings extends Component {
+  render() {
+    return (
+      <header>
+        <h1>Configurações</h1>
+      </header>
+    );
+  }
+}
