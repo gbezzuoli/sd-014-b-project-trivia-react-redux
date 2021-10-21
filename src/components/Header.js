4c42c0d2d5335fb9cdd3506ac6da33ddab25f073
perchespierri <perchespierri@gmail.com>
2021-10-21 11:33:09 -0300

    A header esta pronta. Falta alinhar as actions e reducer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Header extends Component {
   render() {
+    const { profileImage, name, score } = this.props;
     return (
       <div>
-        CABEÇALHO
+        <h1 data-testid="header-player-name">
+          { name }
+        </h1>
+        <img
+          src={ `https://www.gravatar.com/avatar/${profileImage}` }
+          alt="Foto do perfil"
+          data-testid="header-profile-picture"
+        />
+        <span data-testid="header-score">
+          { score }
+        </span>
       </div>
     );
   }
 }
 
-export default Header;
+Header.propTypes = {
+  name: PropTypes.string.isRequired,
+  profileImage: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  name: state.gameReducers.name,
+  score: state.gameReducers.score,
+  profileImage: state.gameReducers.profileImage,
+
+});
+
+export default connect(mapStateToProps, null)(Header);
