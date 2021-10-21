a12f9f8fb5ae67a0364f2b537f0d00a27ea90287
rphsantos1995 <rph.santos@Outlook.com>
2021-10-21 03:07:15 -0300

    Req 5 e 6 ?

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -4,12 +4,15 @@ import {
   Route,
 } from 'react-router-dom';
 import Login from '../pages/Login';
+import GamePage from '../pages/GamePage';
 
 class Routes extends Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route path="/paginadojogo" component={ GamePage } />
+
       </Switch>
     );
   }
