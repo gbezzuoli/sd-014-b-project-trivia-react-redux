c8da4b7fd53062f02d06d0230c446b8947aebd76
Vitor Silva <vitor.o.silva95@gmail.com>
2021-10-21 18:59:36 -0300

    Requisito 5 e 6 , passando

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -3,21 +3,36 @@ import { connect } from 'react-redux';
 import { resultAsk } from '../actions/actionTypes';
 
 class GamePage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      index: 0,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
   componentDidMount() {
     const { dispatchAskGame } = this.props;
     dispatchAskGame();
   }
 
+  handleClick() {
+    this.setState((prevState) => ({ index: prevState.index + 1 }));
+  }
+
   render() {
     const { question } = this.props;
+    const { index } = this.state;
     console.log(question);
     if (!question) {
       return <h1>Loading ...</h1>;
     }
     return (
       <div>
-        <h2 data-testid="question-category ">{ question[0].category}</h2>
-        <h3 data-testid="question-text ">{}</h3>
+        <h2 data-testid="question-category">{ question[index].category}</h2>
+        <h3 data-testid="question-text">{ question[index].question}</h3>
+        <button type="button" data-testid="correct-answer" onClick={ this.handleClick }>{question[index].correct_answer }</button>
+        {question[index].incorrect_answers.map((question) => <button type="button" data-testid={ `wrong-answer-${index}` } onClick={ this.handleClick }>{ question }</button>)}
       </div>
     );
   }
