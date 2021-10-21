4f2467af90324b35ac89544c53ec4fe1c5ccc402
perchespierri <perchespierri@gmail.com>
2021-10-21 11:23:24 -0300

    Criaçao do header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        CABEÇALHO
+      </div>
+    );
+  }
+}
+
+export default Header;
