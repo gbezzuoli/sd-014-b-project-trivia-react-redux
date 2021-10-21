5f09208040ba9e44d34ec02af57860d9e1ba9ef7
dineinunes <nunesvieiraclaudinei@outlook.com>
2021-10-21 18:26:58 -0300

    Soluciona requisito 18

---
+++ src/components/Button.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class Button extends React.Component {
+  render() {
+    const { id, label, onClick } = this.props;
+    return (
+      <button
+        data-testid={ id }
+        type="button"
+        onClick={ onClick }
+      >
+        { label }
+      </button>
+    );
+  }
+}
+
+export default connect()(Button);
+
+Button.propTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
+Button.defaultProps = {
+
+};
--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,22 +1,41 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
+import Button from '../components/Button';
 import RankingRows from '../components/RankingRows';
 
 class Ranking extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    return <Redirect to="/" />;
+  }
+
   render() {
     const ranking = localStorage.getItem('ranking');
     return (
-      <div>
-        { JSON.parse(ranking).map((rankingRow) => (
-          <RankingRows
-            key={ rankingRow.id }
-            name={ rankingRow.name }
-            picture={ rankingRow.picture }
-            score={ rankingRow.score }
-          />
-        ))}
-      </div>
+      <section>
+        <div>
+          { JSON.parse(ranking).map((rankingRow) => (
+            <RankingRows
+              key={ rankingRow.id }
+              name={ rankingRow.name }
+              picture={ rankingRow.picture }
+              score={ rankingRow.score }
+            />
+          ))}
+        </div>
+        <Button
+          id="btn-go-home"
+          label="Início"
+          onClick={ this.handleClick }
+        />
+      </section>
     );
   }
 }
