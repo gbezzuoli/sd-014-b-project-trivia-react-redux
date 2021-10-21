907d6413e2cb4bd4fae7539b4cbfa926956c0d4b
dsomarcelo <celoygm@gmail.com>
2021-10-21 15:23:35 -0300

    try 2

--- src/components/QuestionCard.js
+++ src/components/QuestionCard.js
@@ -1,14 +1,28 @@
+// https://flaviocopes.com/how-to-shuffle-array-javascript/
 import React from 'react';
 import PropTypes from 'prop-types';
 
 class QuestionCard extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      answerList:
+    }
+  }
+  
+  listAnswersMultiple = () => {
+    const incorrectAnswers = apiResult.incorrect_answers;
+    const correctAnswer = apiResult.correct_answer;
+    const answerList = [... correctAnswer, incorrectAnswers]
+  }
+
   render() {
     const { questionText, answerList } = this.props;
     return (
       <section className="QuestionCard">
         <h3 className="question">{questionText}</h3>
         {answerList.map((question, index) => (
-          <button type="button" key={ index }>
+          <button type="button" key={ index } onClick={ this.submitAnswer }>
             { question }
           </button>
         ))}
