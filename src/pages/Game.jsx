a943def3888c185a8a1944bd5985d1ca7a1a69e5
Leomar Linhares <leomar_linhares@hotmail.com>
2021-10-21 17:09:13 -0300

    cria função de requisição na page game

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -14,6 +14,7 @@ export default class Game extends React.Component {
     await this.requestTrivia();
   }
 
+  // Função de requisição do Trivia
   async requestTrivia() {
     const token = localStorage.getItem('token');
     const response = await fetch(`https://opentdb.com/api.php?amount=5&token=${token}`);
