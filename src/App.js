9f1e99de7e8d57c2002c473ce0c1a0c2fc5af2a8
anastacioneto <anastacio.a.c.n@gmail.com>
2021-10-21 19:00:55 -0300

    adiciona requisito 3 completo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import logo from './trivia.png';
 import Login from './pages/Login';
 import Game from './pages/Game';
+import Settings from './pages/Settings';
+
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
         <div>
           <Switch>
             <Route path="/game" component={ Game } />
+            <Route path="/settings" component={ Settings } />
             <Route exact path="/" component={ Login } />
           </Switch>
         </div>
---
+++ src/pages/Settings.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Settings extends Component {
+  render() {
+    return (
+      <div data-testid="settings-title">
+        Pagina de configuracao.
+      </div>
+    );
+  }
+}
+
+export default Settings;
