004054f9bf79b4a520df9b01b3245af06be7c9cf
Vitor Lima <vitorliman@gmail.com>
2021-10-21 15:56:31 -0300

    Tentativa requisito 6

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,18 +2,17 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import '../styles/headerstyle.css';
 import PropTypes from 'prop-types';
-import md5 from 'crypto-js/md5';
 
 class Header extends Component {
   render() {
-    const { name } = this.props;
+    const { name, email } = this.props;
 
     return (
       <header className="header-container">
         <image
           data-testid="header-profile-picture"
-          src=""
-          alt="image player"
+          src={ `https://www.gravatar.com/avatar/${email}` }
+          alt={ name }
         />
         <span data-testid="header-player-name">{ name }</span>
         <div data-testid="header-score">0</div>
@@ -24,10 +23,12 @@ class Header extends Component {
 
 Header.propTypes = {
   name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   name: state.login.name,
+  email: state.login.email,
 });
 
 export default connect(mapStateToProps)(Header);
