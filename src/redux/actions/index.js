c7d97e9a01a57b475994bae98c25f96e0561cb9d
belaguarino <izabelaguarino@gmail.com>
2021-10-21 18:47:11 -0300

    Limpeza

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -5,8 +5,6 @@ export const sendPlayerInfo = (payload) => ({
   payload,
 });
 
-// const EMPTY_KEY = [];
-
 export const saveToLocalStorage = (object) => {
   localStorage.setItem('token', object.token);
 };
@@ -14,21 +12,3 @@ export const saveToLocalStorage = (object) => {
 export const fetchAPI = () => fetch('https://opentdb.com/api_token.php?command=request')
   .then((data) => data.json())
   .then((response) => saveToLocalStorage(response));
-
-/* async function fetchToken() {
-  const response = await fetch('https://opentdb.com/api_token.php?command=request');
-  const json = await response.json();
-  const result = json.token;
-  console.log(result);
-  return result;
-} */
-
-/* ∫ */
-
-/*
-};
- */
-/*
-export const loadFromLocalStorage = (key) => (
-  key === 'token' ? localStorage.getItem(key)
-    : JSON.parse(localStorage.getItem(key)) || EMPTY_KEY); */
