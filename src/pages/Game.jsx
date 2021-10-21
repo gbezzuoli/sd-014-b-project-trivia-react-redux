c1bf2ae46583f8bef2f116d775738fb374beb0a6
Thomas Ferreira <weltonthomasferreira@outlook.com>
2021-10-21 04:16:17 -0300

    Cria um header que deve conter as informações da pessoa jogadora

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchAvatar } from '../redux/slices/userSlice';
 
 export default function Game() {
-  return <>Game Page</>;
+  const dispatch = useDispatch();
+  const { name, email, avatar } = useSelector((store) => store.user);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      dispatch(fetchAvatar(email));
+    };
+    fetchAPI();
+  }, [dispatch, email]);
+
+  return (
+    <>
+      <header>
+        <img data-testid="header-profile-picture" src={ avatar } alt="" />
+        <span data-testid="header-player-name">{name}</span>
+        <span data-testid="header-score">{`Pontos: ${0}`}</span>
+      </header>
+      <main>Game Page</main>
+    </>
+  );
 }
--- src/redux/slices/userSlice.js
+++ src/redux/slices/userSlice.js
@@ -1,15 +1,28 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import md5 from 'crypto-js/md5';
 
 const initialState = {
   name: '',
   email: '',
+  avatar: '',
   tokenAPI: {},
 };
 
 export const fetchToken = createAsyncThunk('user/fetchToken', async () => {
-  const response = fetch('https://opentdb.com/api_token.php?command=request');
+  const response = await fetch(
+    'https://opentdb.com/api_token.php?command=request',
+  );
   return (await response).json();
 });
+
+export const fetchAvatar = createAsyncThunk(
+  'user/fetchAvatar',
+  async (email) => {
+    const hash = md5(email).toString();
+    const response = await fetch(`https://www.gravatar.com/avatar/${hash}`);
+    return response.url;
+  },
+);
 
 export const userSlice = createSlice({
   name: 'login',
@@ -21,10 +34,17 @@ export const userSlice = createSlice({
     },
   },
   extraReducers: (builder) => {
-    builder.addCase(fetchToken.fulfilled, (state, action) => {
-      state.tokenAPI = action.payload;
-      localStorage.setItem('token', JSON.stringify(action.payload.token));
-    });
+    builder
+      .addCase(fetchToken.fulfilled, (state, action) => {
+        state.tokenAPI = action.payload;
+        localStorage.setItem('token', JSON.stringify(action.payload.token));
+      })
+      .addCase(fetchAvatar.fulfilled, (state, action) => {
+        state.avatar = action.payload;
+      })
+      .addCase(fetchAvatar.rejected, (state, action) => {
+        state.avatar = action.payload;
+      });
   },
 });
 
