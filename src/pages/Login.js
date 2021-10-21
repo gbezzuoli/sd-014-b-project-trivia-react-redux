579c11072f08aaec9ac7c3883ce7c2da14544bca
perchespierri <perchespierri@gmail.com>
2021-10-21 08:17:52 -0300

    Criada a action para pegar email e nome

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { getGameTokenAction , getNameAndEmailAction } from '../redux/actions';
+import { getGameTokenAction, getNameAndEmailAction } from '../redux/actions';
 
 import fetchToken from '../services/FetchToken';
 
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,6 +1,13 @@
 export const GET_TOKEN = 'GET_TOKEN';
+export const GET_NAME_AND_EMAIL = 'GET_NAME_AND_EMAIL';
 
 export const getGameTokenAction = (payload) => ({
   type: GET_TOKEN,
   payload,
+});
+
+export const getNameAndEmailAction = (name, email) => ({
+  type: 'GET_NAME_AND_EMAIL',
+  name,
+  email,
 });
