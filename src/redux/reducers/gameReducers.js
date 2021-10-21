ea4b43c2362b555c20f8e5825bf4fc31c26c8991
perchespierri <perchespierri@gmail.com>
2021-10-21 08:20:54 -0300

    Criado o reducer de nome e email

--- src/redux/reducers/gameReducers.js
+++ src/redux/reducers/gameReducers.js
@@ -1,15 +1,23 @@
-import { GET_TOKEN } from '../actions';
+import { GET_TOKEN, GET_NAME_AND_EMAIL } from '../actions';
 
 const INITIAL_STATE = {
   token: '',
+  name: '',
+  email: '',
 };
 
-export default function gameReducer(state = INITIAL_STATE, action) {
+export default function gameReducers(state = INITIAL_STATE, action) {
   switch (action.type) {
   case GET_TOKEN:
     return {
       ...state,
       token: action.payload,
+    };
+  case GET_NAME_AND_EMAIL:
+    return {
+      ...state,
+      name: action.name,
+      email: action.email,
     };
   default:
     return state;
