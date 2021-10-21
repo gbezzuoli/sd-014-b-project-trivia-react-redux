99d37ba4ec23ec101dd910c68c2f3bc7bfe736ef
perchespierri <perchespierri@gmail.com>
2021-10-21 18:48:41 -0300

    Iniciando req 9

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -11,7 +11,7 @@ class Game extends Component {
       questions: '',
       correctAnswer: '',
       // answered: false,
-      count: 0,
+      count: -1,
       disabledState: false,
       currentTime: 35,
     };
