beeb5e2db990405f505d4445a074dd9a8b04e8c6
Bernardo Raffide <bernardoraffide@pop-os.localdomain>
2021-10-22 18:33:38 -0300

    agora vai

---
+++ src/components/PlayAgainButton.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class PlayAgainButton extends Component {
+  constructor() {
+    super();
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const { history } = this.props;
+    history.push('/');
+  }
+
+  render() {
+    return (
+      <div>
+        <button
+          type="button"
+          data-testid="btn-play-again"
+          onClick={ this.handleClick }
+
+        >
+          Jogar novamente
+
+        </button>
+      </div>
+    );
+  }
+}
+
+PlayAgainButton.propTypes = {
+  history: PropTypes.arrayOf(Object).isRequired,
+};
+
+export default PlayAgainButton;
