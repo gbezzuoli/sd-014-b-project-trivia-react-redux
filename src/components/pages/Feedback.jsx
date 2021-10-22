d651dcebf6c0c8add79b21628148f07a83da0cda
Ana-Alice-Honorio <ana.alice.honorio@gmail.com>
2021-10-22 15:40:55 -0300

    req 13

--- src/components/pages/Feedback.jsx
+++ src/components/pages/Feedback.jsx
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from './Header';
 
 class Feedback extends Component {
+  constructor() {
+    super();
+    this.state = {
+      goodResult: 'Mandou bem!',
+      badResult: 'Podia ser melhor...',
+      questionsAssert: 3,
+    };
+  }
+
   render() {
+    const { goodResult, badResult, questionsAssert } = this.state;
+    const { score } = this.props;
     return (
       <section>
         <Header />
+        <h3 data-testid="feedback-text">
+          { questionsAssert >= score ? goodResult : badResult }
+        </h3>
       </section>
     );
   }
 }
 
-export default Feedback;
+Feedback.propTypes = {
+  score: PropTypes.number,
+}.isRequired;
+
+const mapStateToProps = (state) => ({
+  score: state.userReducer.score,
+});
+
+export default connect(
+  mapStateToProps,
+)(Feedback);
