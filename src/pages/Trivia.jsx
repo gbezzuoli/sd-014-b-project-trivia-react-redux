282a8bda19d5d9e52f7822cfe01880f34473f524
Lucas Alves <lucalves.rodrigues@gmail.com>
2021-10-22 18:59:06 -0300

    Requisito 7

--- src/pages/Trivia.jsx
+++ src/pages/Trivia.jsx
@@ -6,15 +6,29 @@ import Header from '../Component/Header';
 class Trivia extends Component {
   constructor() {
     super();
+    this.handleColorClick = this.handleColorClick.bind(this);
+
     this.state = {
       questionIndex: 0,
     };
   }
 
+  // consultado o pr do grupo 9
+  handleColorClick() {
+    const getAllButtons = document.querySelectorAll('button');
+    getAllButtons.forEach((btn) => {
+      if (btn.value === 'wrong-ans') {
+        btn.style.border = '3px solid rgb(255, 0, 0)';
+      }
+      if (btn.value === 'correct-ans') {
+        btn.style.border = '3px solid rgb(6, 240, 15)';
+      }
+    });
+  }
+
   render() {
     const { receviQuestions } = this.props;
     const { questionIndex } = this.state;
-
     return (
       <div>
         <Header />
@@ -24,7 +38,9 @@ class Trivia extends Component {
           <div key={ index }>
             <button
               data-testid={ `wrong-answer-${index}` }
+              onClick={ this.handleColorClick }
               type="button"
+              value="wrong-ans"
             >
               {question}
             </button>
@@ -32,7 +48,9 @@ class Trivia extends Component {
         ))}
         <button
           data-testid="correct-answer"
+          onClick={ this.handleColorClick }
           type="button"
+          value="correct-ans"
         >
           { receviQuestions[questionIndex].correct_answer }
         </button>
