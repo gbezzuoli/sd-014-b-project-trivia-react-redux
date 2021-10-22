477cb7c06addd91823bcc2fa28d8fe4a64132220
Juliana Braga <juliana4braga@gmail.com>
2021-10-21 21:29:55 -0300

    Cria style da pagina de Login

--- src/components/ButtonConfig.js
+++ src/components/ButtonConfig.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { FaCog } from 'react-icons/fa';
+import '../pages/LoginPage.css';
 
 class ButtonConfig extends Component {
   render() {
     const { redirectConfig } = this.props;
     return (
       <button
+        className="btn-settings"
         data-testid="btn-settings"
         type="button"
         onClick={ redirectConfig }
       >
-        Configurações
+        <FaCog />
       </button>
     );
   }
