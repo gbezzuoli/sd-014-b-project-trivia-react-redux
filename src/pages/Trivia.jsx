9488c6b30133f678caaa5f33c9b1ff681eaef955
Renato Bispo <renatoslvbispo@gmail.com>
2021-10-22 17:21:36 -0300

    Finaliza requisitos 5 e 6

--- src/pages/Trivia.jsx
+++ src/pages/Trivia.jsx
@@ -2,7 +2,6 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Header from '../Component/Header';
-import { fetchQuestions } from '../redux/actions';
 
 class Trivia extends Component {
   constructor() {
@@ -12,23 +11,10 @@ class Trivia extends Component {
     };
   }
 
-  componentDidMount() {
-    const { getQuestions } = this.props;
-    getQuestions();
-  }
-
   render() {
     const { receviQuestions } = this.props;
     const { questionIndex } = this.state;
-    const haveData = receviQuestions.length > 0;
-    if (haveData === false) {
-      return (
-        <div>
-          <Header />
-          <span>Loading</span>
-        </div>
-      );
-    }
+
     return (
       <div>
         <Header />
@@ -56,18 +42,13 @@ class Trivia extends Component {
 }
 
 Trivia.propTypes = {
-  getQuestions: PropTypes.func.isRequired,
   receviQuestions: PropTypes.shape({
     length: PropTypes.number,
   }).isRequired,
 };
 
-const mapDispatchToProps = (dispatch) => ({
-  getQuestions: (payload) => dispatch(fetchQuestions(payload)),
-});
-
 const mapStateToProps = (state) => ({
   receviQuestions: state.trivia.questions,
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(Trivia);
+export default connect(mapStateToProps)(Trivia);
