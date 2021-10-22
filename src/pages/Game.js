7b7cbfb27fa1424e646b96f2ef9c2f1f08ca09b7
Diogo Fiuza <difipalm@gmail.com>
2021-10-22 16:56:12 -0300

    Requisito 5 e 6

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -6,70 +6,6 @@ import CorrectAnswer from '../components/CorrectAnswer';
 import GameHeader from '../components/GameHeader';
 import { fecthTrivia } from '../redux/actions';
 
-// const OBJ_TESTE = {
-//   response_code: 0,
-//   results: [
-//     {
-//       category: 'Entertainment: Japanese Anime & Manga',
-//       type: 'boolean',
-//       difficulty: 'easy',
-//       question: 'In the 1988 film &quot;Akira&quot;, Tetsuo ends up destroying Tokyo.',
-//       correct_answer: 'True',
-//       incorrect_answers: [
-//         'False',
-//       ],
-//     },
-//     {
-//       category: 'Geography',
-//       type: 'multiple',
-//       difficulty: 'medium',
-//       question: 'The Japanese district Akihabara is also known by what nickname?',
-//       correct_answer: 'Electric Town',
-//       incorrect_answers: [
-//         'Moon Walk River',
-//         'Otaku Central ',
-//         'Big Eyes',
-//       ],
-//     },
-//     {
-//       category: 'Entertainment: Video Games',
-//       type: 'multiple',
-//       difficulty: 'easy',
-//       question: 'Who is the leader of Team Mystic in Pok&eacute;mon Go?',
-//       correct_answer: 'Blanche',
-//       incorrect_answers: [
-//         'Candela',
-//         'Spark',
-//         'Willow',
-//       ],
-//     },
-//     {
-//       category: 'Science & Nature',
-//       type: 'multiple',
-//       difficulty: 'hard',
-//       question: 'Where is the Gluteus Maximus muscle located?',
-//       correct_answer: 'Butt',
-//       incorrect_answers: [
-//         'Arm',
-//         'Head',
-//         'Torso',
-//       ],
-//     },
-//     {
-//       category: 'Entertainment: Comics',
-//       type: 'multiple',
-//       difficulty: 'easy',
-//       question: 'What is the full first name of the babysitter in Calvin and Hobbes?',
-//       correct_answer: 'Rosalyn',
-//       incorrect_answers: [
-//         'Rose',
-//         'Ruby',
-//         'Rachel',
-//       ],
-//     },
-//   ],
-// };
-
 class Game extends Component {
   constructor() {
     super();
@@ -77,6 +13,7 @@ class Game extends Component {
       questions: [],
       index: 0,
       next: false,
+      loading: true,
     };
 
     this.renderQuestionsRandomAnswers = this.renderQuestionsRandomAnswers.bind(this);
@@ -85,15 +22,18 @@ class Game extends Component {
     this.fetchQuestionsState = this.fetchQuestionsState.bind(this);
   }
 
-  componentDidMount() {
-    this.fetchQuestionsState();
+  async componentDidMount() {
+    await this.fetchQuestionsState();
   }
 
   async fetchQuestionsState() {
     const dataQuestions = await fecthTrivia();
     const { results } = dataQuestions;
-    console.log(dataQuestions);
-    this.setState({ questions: results });
+    console.log(results);
+    this.setState({
+      questions: results,
+      loading: false,
+    });
   }
 
   answerClickHandler(event) {
@@ -136,7 +76,8 @@ class Game extends Component {
   }
 
   render() {
-    const { questions, index, next } = this.state;
+    const { loading, questions, index, next } = this.state;
+    if (loading) return <h1>Loading</h1>;
     return (
       <div>
         <GameHeader />
