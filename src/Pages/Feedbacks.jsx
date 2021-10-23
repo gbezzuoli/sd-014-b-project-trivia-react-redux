b190b3208b5c3d0ebea7dfea653a41b2b0dbba0f
Gustavo Sant'Anna <g_santanna@outlook.com>
2021-10-23 13:44:14 -0300

    Requisito 15 iniciado

--- src/Pages/Feedbacks.jsx
+++ src/Pages/Feedbacks.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Feedbacks extends Component {
   render() {
     return (
       <section>
         <p data-testid="feedback-text">Feedbacks</p>
+        <Link to="/">
+          <button type="button" data-testid="btn-play-again">
+            Play Again
+          </button>
+        </Link>
       </section>
     );
   }
