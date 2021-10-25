294b36002b6cdf6eab3c99abb06740d8e962ac1a
Lucas Alves <lucalves.rodrigues@gmail.com>
2021-10-25 17:40:28 -0300

    Requisito 14

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -7,16 +7,54 @@ class Feedback extends Component {
   constructor(props) {
     super(props);
 
+    this.feedbackMessages = this.feedbackMessages.bind(this);
+
     this.state = {
     };
   }
 
   feedbackMessages() {
-    const { assertions } = this.props;
+    const { assertions, score } = this.props;
     const NUMBER = 3;
-    if (assertions <= NUMBER) {
-      return (<h1>Podia ser melhor...</h1>);
-    } return (<h1>Mandou bem!</h1>);
+    if (assertions <= NUMBER && assertions > 0) {
+      return (
+        <>
+          <h1>Podia ser melhor...</h1>
+          <h2 data-testid="feedback-total-score">
+            Placar final:
+            {' '}
+            { score }
+          </h2>
+          <h3 data-testid="feedback-total-question">
+            Você acertou:
+            {' '}
+            { assertions }
+          </h3>
+        </>
+      );
+    }
+    if (assertions >= NUMBER) {
+      return (
+        <>
+          <h1>Mandou bem!</h1>
+          <h2 data-testid="feedback-total-score">
+            Placar final:
+            {' '}
+            { score }
+          </h2>
+          <h3 data-testid="feedback-total-question">
+            Você acertou:
+            {' '}
+            { assertions }
+          </h3>
+        </>
+      );
+    }
+    if (assertions === 0) {
+      return (
+        <h1 data-testid="feedback-total-question">Não acertou nenhuma pergunta</h1>
+      );
+    }
   }
 
   render() {
@@ -31,10 +69,12 @@ class Feedback extends Component {
 
 Feedback.propTypes = {
   assertions: PropTypes.number.isRequired,
+  score: PropTypes.number.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   assertions: state.assertions,
+  score: state.score,
 });
 
 export default connect(mapStateToProps)(Feedback);
