6c2c6bbb4c55909989c0f1e6098a4ed7acedc551
luizsmatos <gusstavopdm@gmail.com>
2021-10-25 16:27:30 -0300

    Requisito 13 - Concluido

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -2,12 +2,41 @@ import React from 'react';
 import Header from '../components/Header/Header';
 
 class Feedback extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      player: {},
+      loading: true,
+    };
+    this.saveState = this.saveState.bind(this);
+  }
+
+  componentDidMount() {
+    const { player } = JSON.parse(localStorage.getItem('state'));
+
+    if (player) {
+      this.saveState(player);
+    }
+  }
+
+  saveState(player) {
+    this.setState({ player, loading: false });
+  }
+
   render() {
+    const { player, loading } = this.state;
+    const magicNumber = 3;
     return (
-      <div>
+      <>
         <Header />
-        <h1 data-testid="feedback-text">Feed Back</h1>
-      </div>
+        <div>
+          <h3 data-testid="feedback-text">
+            { !loading && player.assertions < magicNumber
+              ? 'Podia ser melhor...' : 'Mandou bem!' }
+          </h3>
+        </div>
+      </>
     );
   }
 }
