c4401db41782c5ffd42f2a2a1d24130146fc61d9
Anderson Leite <andersonleite.dev@gmail.com>
2021-10-25 17:10:48 -0300

    Requesito 14

--- src/components/FeedbackText.js
+++ src/components/FeedbackText.js
@@ -21,20 +21,53 @@ class FeedbackText extends React.Component {
     const { numberHits } = this.props;
 
     if (numberHits < numberMinHits) {
-      this.setState({ message: 'Podia ser melhor...' });
+      this.setState({
+        message: 'Podia ser melhor...',
+      });
     } else {
-      this.setState({ message: 'Mandou bem!' });
+      this.setState({
+        message: 'Mandou bem!',
+      });
     }
   }
 
   render() {
     const { message } = this.state;
-    return <h4 data-testid="feedback-text">{`FeedBack ${message}`}</h4>;
+    const { numberHits, totalScore } = this.props;
+
+    return (
+      <>
+        <section>
+          <h4 data-testid="feedback-text">{`${message}`}</h4>
+        </section>
+        <section>
+          <h4>
+            Total de pontos:
+            {' '}
+            <span data-testid="feedback-total-score">{totalScore}</span>
+          </h4>
+        </section>
+        <section>
+          {numberHits ? (
+            <h4>
+              Acertou
+              {' '}
+              <span data-testid="feedback-total-question">{numberHits}</span>
+              {' '}
+              perguntas
+            </h4>
+          ) : (
+            <h4 data-testid="feedback-total-question">Não acertou nenhuma pergunta</h4>
+          )}
+        </section>
+      </ >
+    );
   }
 }
 
 FeedbackText.propTypes = {
   numberHits: PropTypes.number.isRequired,
+  totalScore: PropTypes.number.isRequired,
 };
 
 export default FeedbackText;
--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,16 +1,29 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import FeedbackText from '../components/FeedbackText';
 import Header from '../components/Header';
 
 class Feedback extends Component {
   render() {
+    const { score, assertions } = this.props;
     return (
       <div>
         <Header />
-        <FeedbackText numberHits={ 2 } />
+        <FeedbackText numberHits={ assertions } totalScore={ score } />
       </div>
     );
   }
 }
 
-export default Feedback;
+Feedback.propTypes = {
+  assertions: PropTypes.number.isRequired,
+  score: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  assertions: state.player.assertions,
+  score: state.player.score,
+});
+
+export default connect(mapStateToProps)(Feedback);
