36d3c45d92dd8a392d07df24352b191337e56d98
Thomas Ferreira <weltonthomasferreira@outlook.com>
2021-10-25 19:41:14 -0300

    Implementa requisito 12

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Feedback extends Component {
   render() {
+    const three = 3;
+    const { avatar, name } = this.props;
+    const { assertions, score } = JSON.parse(
+      localStorage.getItem('state'),
+    ).player;
     return (
-      <>
-        Feedback
-        <h1 data-testid="feedback-text">Resultado</h1>
-      </>
+      <div>
+        <header>
+          <img data-testid="header-profile-picture" src={ avatar } alt="" />
+          <span data-testid="header-player-name">{name}</span>
+          Pontos:
+          <span data-testid="header-score">{score}</span>
+        </header>
+        {assertions <= three ? (
+          <p data-testid="feedback-text">Podia ser melhor...</p>
+        ) : (
+          <p data-testid="feedback-text">Mandou Bem!</p>
+        )}
+      </div>
     );
   }
 }
-
-export default Feedback;
+const mapStateToProps = (state) => ({
+  name: state.user.name,
+  email: state.user.email,
+  avatar: state.user.avatar,
+  isGameReady: state.game.isGameReady,
+  playerScore: state.user.score,
+});
+Feedback.propTypes = {
+  name: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+};
+export default connect(mapStateToProps)(Feedback);
