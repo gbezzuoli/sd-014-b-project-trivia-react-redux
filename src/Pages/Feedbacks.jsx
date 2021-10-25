93546d1ad64a97817fbe705c2e346cebf3cb5672
Antônio Gonçalves <tonygoldmarket@yahoo.com>
2021-10-25 15:41:19 -0300

    Requisito 14 concluido

--- src/Pages/Feedbacks.jsx
+++ src/Pages/Feedbacks.jsx
@@ -6,15 +6,29 @@ import Header from '../Components/Header';
 
 class Feedbacks extends Component {
   render() {
-    const { assertions } = this.props;
+    const { score, assertions } = this.props;
     const THREE = 3;
     return (
       <section>
         <Header />
         <h2 data-testid="feedback-text">Feedbacks</h2>
-        { assertions < THREE
-          ? <p data-testid="feedback-text">Podia ser melhor...</p>
-          : <p data-testid="feedback-text">Mandou bem!</p> }
+        <section className="feedback-section">
+          { assertions < THREE
+            ? <p data-testid="feedback-text">Podia ser melhor...</p>
+            : <p data-testid="feedback-text">Mandou bem!</p> }
+          <p>
+            Acertou:
+            <span data-testid="feedback-total-question">
+              { assertions }
+            </span>
+          </p>
+          <p>
+            Pontos:
+            <span data-testid="feedback-total-score">
+              { score }
+            </span>
+          </p>
+        </section>
         <Link to="/">
           <button type="button" data-testid="btn-play-again">Play Again</button>
         </Link>
@@ -28,9 +42,11 @@ class Feedbacks extends Component {
 
 Feedbacks.propTypes = {
   assertions: PropTypes.number.isRequired,
+  score: PropTypes.number.isRequired,
 };
 
 const mapStateToProps = (state) => ({
+  score: state.player.score,
   assertions: state.player.assertions,
 });
 
